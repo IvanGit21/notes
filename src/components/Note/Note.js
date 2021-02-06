bf5fa5e3ddeed10b97584cdad558b8b62f7a310c
Ivan Kondratyev <zm.fut@bk.ru>
2021-02-06 12:59:37 +0300

    fix

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,1 +1,29 @@
-p
+import React from "react";
+import "./Note.css";
+import closeIcon from "../../images/close.svg";
+
+function Note({ data: { title, text }, i, onDeleteNote, onViewClick }) {
+  function handleDeleteClick() {
+    onDeleteNote(i);
+  }
+  function handleViewClick() {
+    onViewClick({ title: title, text: text });
+  }
+
+  return (
+    <div className="note__container">
+      <img
+        className="note__close-icon"
+        src={closeIcon}
+        alt="close-icon"
+        onClick={handleDeleteClick}
+      />
+      <div onClick={handleViewClick}>
+        <h1 className="note__title">{title}</h1>
+        <p className="note__text">{text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
